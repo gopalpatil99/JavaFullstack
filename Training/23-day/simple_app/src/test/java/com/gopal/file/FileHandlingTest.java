26ef53bf134f5e118b99259012c2a869178be362
gopalpatil99 <gopalpatil99@gmail.com>
2023-03-25 18:37:01 +0530

    mvn project

---
+++ Training/23-day/simple_app/src/test/java/com/gopal/file/FileHandlingTest.java
@@ -0,0 +1,15 @@
+package com.gopal.file;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FileHandlingTest {
+    
+    @Test
+    public void testFileExistsOrNot() {
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            FileHandling fh = new FileHandling();
+            fh.readFile("abc.txt");
+        });
+    }
+}
