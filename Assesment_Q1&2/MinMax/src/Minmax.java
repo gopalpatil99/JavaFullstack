e8e3ae5cd24d63c691ba26c01e63f35d2a9c6665
gopalpatil99 <gopalpatil99@gmail.com>
2023-03-28 10:32:00 +0530

    code

---
+++ Assesment_Q1&2/MinMax/src/Minmax.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+public class Minmax {
+  public static void main(String[] args) {
+    int[] arr = {1, 2, 3, 4, 5};
+    int min = Arrays.stream(arr).min().getAsInt();
+    int max = Arrays.stream(arr).max().getAsInt();
+    double avg = Arrays.stream(arr).average().getAsDouble();
+    System.out.println();
+    System.out.println(min);
+    System.out.println();
+    System.out.println(max) ;
+    System.out.println(avg);
+  }
+}
