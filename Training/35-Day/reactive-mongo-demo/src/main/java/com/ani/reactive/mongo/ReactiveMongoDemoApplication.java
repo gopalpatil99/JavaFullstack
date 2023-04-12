26a50f04afa4f171f8af9cb7ed836c0658526f2b
gopalpatil99 <gopalpatil99@gmail.com>
2023-04-12 18:00:33 +0530

    netty http server

---
+++ Training/35-Day/reactive-mongo-demo/src/main/java/com/ani/reactive/mongo/ReactiveMongoDemoApplication.java
@@ -0,0 +1,45 @@
+package com.ani.reactive.mongo;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class ReactiveMongoDemoApplication {
+
+	public static void main(String[] args) throws InterruptedException {
+		ApplicationContext ctx = SpringApplication.run(ReactiveMongoDemoApplication.class, args);
+
+		CountDownLatch latch = new CountDownLatch(2);
+
+		ItemRepository repository = ctx.getBean(ItemRepository.class);
+
+		Item item = new Item(10L, "abc", "aaa", 456.90);
+
+		repository.save(item)
+				.doOnNext(savedItem -> {
+					System.out.println("🔥 Save Completed 🔥");
+					System.out.println(savedItem);
+					System.out.println("👉 One");
+					// latch.countDown();
+				})
+				.doOnError(err -> System.out.println(err))
+				.subscribe();
+
+		repository.findAll()
+				.doOnNext(itm -> {
+					System.out.println("🔥 FindAll Completed 🔥");
+					System.out.println(itm);
+					// latch.countDown();
+				})
+				.doOnError(err -> System.out.println(err))
+				.subscribe();
+
+		System.out.println("👉 Two");
+
+		latch.await();
+	}
+
+}
