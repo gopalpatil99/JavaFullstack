c420430e6ecd8efcb67f7e0413d7718eed2abe19
gopalpatil99 <gopalpatil99@gmail.com>
2023-04-18 10:47:03 +0530

    operators : modification

--- Training/Day8/42-Day/association-mappings/src/main/java/com/ani/mapping/repository/ContactDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ani.mapping.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ani.mapping.domain.ContactDetails;
-
-public interface ContactDetailsRepository extends JpaRepository<ContactDetails, Long>{
-    
-}
--- Training/Day8/42-Day/association-mappings/src/main/java/com/ani/mapping/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ani.mapping.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ani.mapping.domain.Course;
-
-public interface CourseRepository extends JpaRepository<Course, Long>{
-    
-}
