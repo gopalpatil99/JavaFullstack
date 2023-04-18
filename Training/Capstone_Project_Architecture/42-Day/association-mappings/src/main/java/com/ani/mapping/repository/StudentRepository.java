bc7916fbb7632c32bf1038f2410bac26755c1d3f
gopalpatil99 <gopalpatil99@gmail.com>
2023-04-18 10:38:06 +0530

    safe commit

--- Training/Capstone_Project_Architecture/42-Day/association-mappings/src/main/java/com/ani/mapping/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ani.mapping.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ani.mapping.domain.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long>{
-    
-}
